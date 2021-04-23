38af2aa03765cabe8af0b28ca0c93f880089cd6a
Anastasia Makarshina <makarshinaa1@gmail.com>
2021-04-23 18:21:17 +0300

    no message

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform player;
+    public float speed;
+    public float maxBound, minBound;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
+    void Start()
+    {
+        player = GetComponent<Transform>();
+    }
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if (player.position.x < minBound && h < 0)
+            h = 0;
+        else if (player.position.x > maxBound && h > 0)
+            h = 0;
+
+        player.position += Vector3.right * h * speed;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+
+    
+    
+}
